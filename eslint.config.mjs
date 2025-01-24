906cef5d895772555c893d1dcacc2fe762cd58a3
gusfat99 <fatwanbagus.99@gmail.com>
2025-01-24 09:54:21 +0700

    eslint rules set

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", // Nonaktifkan no-explicit-any
+      "@typescript-eslint/no-unused-vars": "off", // Nonaktifkan no-unused-vars
+      "@typescript-eslint/no-unused-expressions" : "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/ban-ts-comment": "off", // Nonaktifkan aturan ban-ts-comment
+    },
+  },
 ];
 
 export default eslintConfig;
